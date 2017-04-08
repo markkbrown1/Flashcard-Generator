f7f74617ba9f79c879f47bb004f5f2283bb27c86
mark <markkbrown1@gmail.com>
2017-04-08 01:18:41 -0400

    changes but Still not working

--- basiccard.js
+++ basiccard.js
@@ -13,6 +13,16 @@ var BasicCards = function(front, back){
 	}
 };
 
+// var firstPresident = new BasicCard(
+//     "Who was the first president of the United States?", "George Washington");
+
+// // "Who was the first president of the United States?"
+// console.log(firstPresident.front); 
+
+// // "George Washington"
+// console.log(firstPresident.back); 
+
+
 
 // exporting our basic cards constructor. We will require it in main.js
 module.exports = BasicCards;
--- clozecard.js
+++ clozecard.js
@@ -3,7 +3,13 @@ var ClozeCards = function(text, cloze){
 	this.text = text;
 	this.cloze = cloze;
 	this.partial = text.replace(cloze, '...');
+	// write and if statement for ClozeCard should throw or log an error when the cloze deletion does not appear in the input text
 	this.clozeGame = function(){
+		if (!text.includes(cloze)) {
+		console.log('ERROR: cloze-deletion did not happen-- <' + cloze + '>');
+		return;
+	}
+};
 		
 
 
